2bfda901a756bc7cc8f3267d78a32696bcaada63
Jules THUYSBAERT <jthuysba@e1r11p15.clusters.42paris.fr>
2022-10-17 18:52:02 +0200

    colors to check

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 12:28:03 by jthuysba          #+#    #+#             */
-/*   Updated: 2022/10/17 16:44:11 by jthuysba         ###   ########.fr       */
+/*   Updated: 2022/10/17 18:48:39 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,53 @@ int	check_file(char *file)
 		return (0);
 }
 
+//Place un pixel
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
+//Initialise une fenetre
 t_mlx	mlx_win_init(void)
 {
 	t_mlx	mlx;
 	t_data	img;
 
 	mlx.mlx_ptr = mlx_init();
-	mlx.mlx_win = mlx_new_window(mlx.mlx_ptr, 640, 360, "FdF");
-	img.img = mlx_new_image(mlx.mlx_ptr, 640, 360);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	mlx.mlx_win = mlx_new_window(mlx.mlx_ptr, 1920, 1080, "FdF");
+	img.img = mlx_new_image(mlx.mlx_ptr, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
 	mlx.img = img;
-	// my_mlx_pixel_put(&img, 10, 10, 0x00FF0000);
-	// mlx_put_image_to_window(mlx.mlx_ptr, mlx_win, img.img, 0, 0);
 	return (mlx);
+}
+
+//Dessine une ligne du fichier
+void	mlx_draw_row(t_mlx mlx, char *line, int y)
+{
+	int		x;
+	char	**arr;
+	char	**parts;
+	char	*color;
+
+	x = 0;
+	arr = ft_split(line, ' ');
+	while (arr[x])
+	{
+		parts = ft_split(arr[x], ',');
+		if (parts[1])
+			color = parts[1];
+		else
+			color = "0xFFFFFFF";
+		if (arr[x][0] == '0')
+			my_mlx_pixel_put(&mlx.img, x, y, (int) color);
+		else
+			my_mlx_pixel_put(&mlx.img, x, y, (int) color);
+		x++;
+	}
 }
 
 int	main(int argc, char **argv)
@@ -54,10 +80,8 @@ int	main(int argc, char **argv)
 	char	*line;
 	int		fd;
 	char	**arr;
-	int		x;
 	int		y;
 
-	x = 0;
 	y = 0;
 	if (argc != 2)
 		return (0);
@@ -68,17 +92,8 @@ int	main(int argc, char **argv)
 	line = get_next_line(fd);
 	while (line)
 	{
-		arr = ft_split(line, ' ');
-		while (arr[x])
-		{
-			if (arr[x][0] == '0')
-				my_mlx_pixel_put(&mlx.img, x, y, 0xFFFFFFF);
-			else
-				my_mlx_pixel_put(&mlx.img, x, y, 0xFFF0909);
-			x++;
-		}
+		mlx_draw_row(mlx, line, y);
 		y++;
-		x = 0;
 		printf("%s", line);
 		line = get_next_line(fd);
 	}
